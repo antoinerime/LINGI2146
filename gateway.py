6a039a32e75f7b337ed5ee4504195f4f5371eaed
antoinerime <antoine.rime@student.uclouvain.be>
2019-05-20 14:12:44 +0200

    Add cli

--- gateway.py
+++ gateway.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-import serial
 import socket
 import os
 import re
+import sys
+
+from threading import Thread, Lock
 
 HOST = '127.0.0.1'
 PORT = 60002
+
+
 
 def handle_line(line):
     regex = r"DATA: (?P<address>\d+.\d+), (?P<topic>\d+.\d+), (?P<metric>\d+.\d+)"
@@ -17,13 +21,37 @@ def handle_line(line):
         metric = match.group('metric')
         os.system('mosquitto_pub -h localhost -p 6000 -t {} -m {}'.format(topic, metric))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    line = ""
+def handle_cmd(s, cmd):
+    if cmd == "periodical_data":
+        s.send("periodical_data\n".encode())
+    elif cmd == "on_change_data":
+        s.send("on_change_data\n".encode())
+    elif cmd == "":
+        pass
+    else:
+        print("Command {} not known\n Try help for the list of commands", CMD)
+
+def cli(socket):
     while True:
-        data = (s.recv(1024)).decode("utf-8")
-        if(data != "\n"):
-            line += data
-        else:
-            line += data
-            handle_line(line)
+        try:
+            cmd = sys.stdin.readline().strip('\n').strip(' ')
+            if cmd == "help":
+                print("Commands are: \n\t periodical_data, \n\t on_change_data")
+            else:
+                handle_cmd(socket, cmd)
+        except Exception as e:
+            print("Caught an exception: {}".format(e))
+
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        t = Thread(target=cli, name="cli", args=(s,), daemon=True)
+        t.start()
+        line = ""
+        while True:
+            data = (s.recv(1024)).decode("utf-8")
+            if(data != "\n"):
+                line += data
+            else:
+                line += data
+                handle_line(line)
